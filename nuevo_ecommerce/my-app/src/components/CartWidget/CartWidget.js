bc6bd3c798feb75bb081f905d711c0bda67ffe22
rafaelgm08 <rafael000182@gmail.com>
2024-01-30 11:53:05 -0500

    nuevo

---
+++ nuevo_ecommerce/my-app/src/components/CartWidget/CartWidget.js
@@ -0,0 +1,12 @@
+import cart from './assets/C:\Users\User\Documents\nuevo_ecommerce\my-app\src\components\CartWidget\assets\shopping-cart-outline-icon-vector.svg'
+const CartWidget = () => {
+    return (
+  <div>
+<img src={cart} alt="cart-widget"/>
+0
+  </div>
+
+    )
+}
+
+export default CartWidget
